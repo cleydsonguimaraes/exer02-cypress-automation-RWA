178b473a3fd537b82ae1f941e220abf4bca0cdb7
Cleydson Guimaraes <cleydsonguimaraes048@gmail.com>
2025-05-19 00:31:02 -0300

    criando arquivo de caso de teste

---
+++ cypress/e2e/enviarDinheiro.spec.cy.js
@@ -0,0 +1,9 @@
+describe('Enviar Dinheiro', () => {
+  it('Deve enviar dinheiro com sucesso', () => {
+    // Implemente os passos do caso de teste aqui
+  });
+
+  it('Deve exibir mensagem de erro ao enviar dinheiro sem saldo suficiente', () => {
+    // Implemente os passos do caso de teste aqui
+  });
+});
